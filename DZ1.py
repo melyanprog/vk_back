7228145aa7181fa8e0234a4a1dcdca563ef40cdc
Sergey Melikyan <eduard-210@mail.ru>
2022-09-25 11:12:17 +0300

    color version

--- DZ1.py
+++ DZ1.py
@@ -5,11 +5,11 @@ class TicTac:
                         (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
 
     def dr_board(self):
-        print('-------------')
+        print("\033[1m{}\033[0m".format('-------------'))
         for i in range(0, 3):
             print('|', self.board[0+i*3], '|',
                   self.board[1+i*3], '|', self.board[2+i*3], '|')
-            print('-------------')
+            print("\033[1m{}\033[0m".format('-------------'))
 
     def valid_input(self, player):
         valid = False
@@ -42,9 +42,9 @@ class TicTac:
         while not win:
             self.dr_board()
             if count % 2 == 0:
-                self.valid_input('X')
+                self.valid_input("\033[34m{}\033[0m".format("X"))
             else:
-                self.valid_input('O')
+                self.valid_input("\033[31m{}\033[0m".format("O"))
             count += 1
             if count > 4:
                 winner = self.check_win()
